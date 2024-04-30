fa6300b074a69b459189c472e7c65564d7601ab2
zerooooooo1 <xushiduo1@gmail.com>
2024-04-30 13:03:56 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { WagmiProvider, http } from 'wagmi'
+import {mainnet, arbitrum, polygon, goerli} from "wagmi/chains";
+import {
+    RainbowKitProvider, getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ConnectButton} from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import {useAccount, useContractRead} from "wagmi";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const account = useAccount()
+
+
+    function confirm() {
+        console.log('aaaa')
+    }
+
+    const config2 = getDefaultConfig({
+        appName: 'RainbowKit demo',
+        projectId: '',
+        chains: [mainnet],
+        transports: {
+            [mainnet.id]: http(),
+        },
+    })
+
+    const queryClient = new QueryClient();
+
+    return (
+        <WagmiProvider config={config2}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider modalSize="compact">
+                    <div className="App">
+                        <header className="App-header">
+                            <ConnectButton/>
+                            <button onClick={() => confirm()}>open metamask</button>
+                        </header>
+                    </div>
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+
+    );
 }
 
 export default App;
